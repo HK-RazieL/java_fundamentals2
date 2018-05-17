b15a3f178c2415c408f5d9a58689514d724a382d
HK-RazieL <s.angelkov@abv.bg>
2018-05-17 12:53:28 +0300

    linear_data_structures-lab

---
+++ java_advanced/linear_data_structures-lab/src/p01_EncryptSortPrintArray/Main.java
@@ -0,0 +1,48 @@
+package p01_EncryptSortPrintArray;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int number = Integer.parseInt(reader.readLine());
+        List<String> name = new ArrayList<>();
+        List<Integer> encryptedStrings = new ArrayList<>();
+
+
+        for (int i = 0; i < number; i++) {
+            name.add(reader.readLine());
+
+        }
+
+        for (int i = 0; i < name.size(); i++) {
+            encryptedStrings.add(encript(name.get(i)));
+        }
+
+        Collections.sort(encryptedStrings);
+
+        for (int encryptedName : encryptedStrings) {
+            System.out.println(encryptedName);
+        }
+    }
+
+    private static int encript (String string) {
+        int sum = 0;
+        for (int i = 0; i < string.length(); i++) {
+            if(isVowel(string.charAt(i))){
+                sum += (int) string.charAt(i) * string.length();
+            } else {
+                sum += (int) string.charAt(i) / string.length();
+            }
+        }
+        return sum;
+    }
+
+    private static boolean isVowel (char letter) {
+        char[] vowels = new char[]{'a', 'e', 'o', 'i', 'u', 'A', 'E', 'O', 'U', 'I'};
+        return Arrays.asList(vowels).contains(letter);
+    }
+}
